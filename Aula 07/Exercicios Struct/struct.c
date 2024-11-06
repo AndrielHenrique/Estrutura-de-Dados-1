b81131ba020c86f1a1127ff2d031aa19a4e641a1
Andriel <andriel.h@aluno.ifsp.edu.br>
2024-11-06 13:32:34 -0300

    Exercicio Struct Finalizado

--- Aula 07/Exercicios Struct/struct.c
+++ Aula 07/Exercicios Struct/struct.c
@@ -12,79 +12,116 @@ typedef struct
     float notas[4];
     int anoInicio;
     int idade;
+    float media;
 } Aluno;
 void cadastrarAluno(int contador, Aluno alunos[MAX])
 {
-    if (contador >= 40)
-        printf("A lista esta cheia!\n");
-    else
-    {
-        printf("Informe os dados do novo aluno: \n");
-        printf("RA: ");
-        scanf("%d", &alunos[contador].ra);
-        printf("Nome: ");
-        getchar();
-        fgets(alunos[contador].nome, sizeof(alunos[contador].nome), stdin);
-        printf("Curso: ");
-        fgets(alunos[contador].curso, sizeof(alunos[contador].curso), stdin);
-        printf("Notas(4): ");
-        scanf("%f %f %f %f", &alunos[contador].notas[0], &alunos[contador].notas[1], &alunos[contador].notas[2], &alunos[contador].notas[3]);
-        printf("Ano de Inicio: ");
-        scanf("%d", &alunos[contador].anoInicio);
-        printf("Idade: ");
-        scanf("%d", &alunos[contador].idade);
+    printf("Informe os dados do novo aluno: \n");
+    printf("RA: ");
+    scanf("%d", &alunos[contador].ra);
+    printf("Nome: ");
+    getchar();
+    fgets(alunos[contador].nome, sizeof(alunos[contador].nome), stdin);
+    printf("Curso: ");
+    fgets(alunos[contador].curso, sizeof(alunos[contador].curso), stdin);
+    printf("Notas(4): ");
+    scanf("%f %f %f %f", &alunos[contador].notas[0], &alunos[contador].notas[1], &alunos[contador].notas[2], &alunos[contador].notas[3]);
+    printf("Ano de Inicio: ");
+    scanf("%d", &alunos[contador].anoInicio);
+    printf("Idade: ");
+    scanf("%d", &alunos[contador].idade);
 
-        printf("Cadastrado com sucesso!\n");
-    }
+    printf("Cadastrado com sucesso!\n");
 }
+
 void listarAlunos(int contador, Aluno aluno[MAX])
 {
-    if (contador > 0)
+    for (int i = 0; i < contador; i++)
     {
-        for (int i = 0; i < contador; i++)
-        {
-            printf("\nRA: %d", aluno[i].ra);
-            printf("\nNome: %s", aluno[i].nome);
-            printf("\nCurso: %s", aluno[i].curso);
-            printf("\nNotas: %.2f %.2f %.2f %.2f", aluno[i].notas[0], aluno[i].notas[1], aluno[i].notas[2], aluno[i].notas[3]);
-            printf("\nMedia das Notas: %.2f", (aluno[i].notas[0] + aluno[i].notas[1] + aluno[i].notas[2] + aluno[i].notas[3]) / 4);
-            printf("\nAno de Inicio: %d", aluno[i].anoInicio);
-            printf("\nIdade: %d", aluno[i].idade);
-        }
+        printf("\nRA: %d", aluno[i].ra);
+        printf("\nNome: %s", aluno[i].nome);
+        printf("\nCurso: %s", aluno[i].curso);
+        printf("\nNotas: %.2f %.2f %.2f %.2f", aluno[i].notas[0], aluno[i].notas[1], aluno[i].notas[2], aluno[i].notas[3]);
+        printf("\nMedia das Notas: %.2f", (aluno[i].notas[0] + aluno[i].notas[1] + aluno[i].notas[2] + aluno[i].notas[3]) / 4);
+        printf("\nAno de Inicio: %d", aluno[i].anoInicio);
+        printf("\nIdade: %d", aluno[i].idade);
     }
-    else
-        printf("Nao existe nenhum aluno cadastrado!\n");
 }
+
 void exibirAluno(int contador, Aluno aluno[MAX])
 {
-    if (contador > 0)
-    {
-        int ra;
-        printf("Informe o RA: ");
-        scanf("%d", &ra);
+    int ra;
+    printf("Informe o RA: ");
+    scanf("%d", &ra);
 
-        for (int i = 0; i < contador; i++)
+    for (int i = 0; i < contador; i++)
+    {
+        if (ra == aluno[i].ra)
         {
-            if (ra == aluno[i].ra)
-            {
-                printf("\nRA: %d\n", aluno[i].ra);
-                printf("Nome: %s\n", aluno[i].nome);
-                printf("Curso: %s\n", aluno[i].curso);
-                printf("Notas: %.2f %.2f %.2f %.2f\n", aluno[i].notas[0], aluno[i].notas[1], aluno[i].notas[2], aluno[i].notas[3]);
-                printf("Media das Notas: %.2f\n", (aluno[i].notas[0] + aluno[i].notas[1] + aluno[i].notas[2] + aluno[i].notas[3]) / 4);
-                printf("Ano de Inicio: %d\n", aluno[i].anoInicio);
-                printf("Idade: %d\n", aluno[i].idade);
-            }
-            else
-            {
-                printf("RA nao encontrado.\n");
-            }
+            printf("\nRA: %d\n", aluno[i].ra);
+            printf("Nome: %s\n", aluno[i].nome);
+            printf("Curso: %s\n", aluno[i].curso);
+            printf("Notas: %.2f %.2f %.2f %.2f\n", aluno[i].notas[0], aluno[i].notas[1], aluno[i].notas[2], aluno[i].notas[3]);
+            printf("Media das Notas: %.2f\n", (aluno[i].notas[0] + aluno[i].notas[1] + aluno[i].notas[2] + aluno[i].notas[3]) / 4);
+            printf("Ano de Inicio: %d\n", aluno[i].anoInicio);
+            printf("Idade: %d\n", aluno[i].idade);
         }
+        else
+            printf("RA nao encontrado.\n");
     }
-    else
-        printf("Nao existe nenhum aluno cadastrado!\n");
 }
 
+void maiorMedia(int contador, Aluno aluno[MAX])
+{
+    int aux = 0;
+    for (int i = 0; i < contador; i++)
+    {
+        aluno[i].media = (aluno[i].notas[0] + aluno[i].notas[1] + aluno[i].notas[2] + aluno[i].notas[3]) / 4;
+        if (aluno[i].media > aluno[aux].media)
+        {
+            aluno[aux] = aluno[i];
+        }
+    }
+    printf("Aluno com a maior media: ");
+    printf("\n\nRA: %d", aluno[aux].ra);
+    printf("\nNome: %s", aluno[aux].nome);
+    printf("\nCurso: %s", aluno[aux].curso);
+    printf("\nNotas: %2f %2f %2f %2f", aluno[aux].notas[0], aluno[aux].notas[1], aluno[aux].notas[2], aluno[aux].notas[3]);
+    printf("\nAno inicio: %d", aluno[aux].anoInicio);
+    printf("\nIdade: %d", aluno[aux].idade);
+    printf("\nMedia do aluno: %.2f", aluno[aux].media);
+}
+
+void mediaDasMedias(int contador, Aluno aluno[MAX])
+{
+    float medias;
+    for (int i = 0; i < contador; i++)
+    {
+        medias += aluno[i].media;
+    }
+    printf("Media das medias: %.2f", medias / contador);
+}
+void excluirAlunos(int contador, Aluno aluno[MAX])
+{
+    int ra, encontrado;
+    printf("Informe o RA do aluno para excluir: ");
+    scanf("%d", &ra);
+    for (int i = 0; i < contador; i++)
+    {
+        if (ra == aluno[i].ra)
+        {
+            encontrado = 1;
+            for (int j = 0; j < contador - 1; j++)
+            {
+                aluno[j] = aluno[j + 1];
+            }
+            printf("Aluno excluido com sucesso!\n");
+            break;
+        }
+    }
+    if (!encontrado)
+        printf("Nao existe aluno com esse RA!\n");
+}
 int main()
 {
     int op;
@@ -104,24 +141,44 @@ int main()
         switch (op)
         {
         case 1:
-            cadastrarAluno(contador, alunos);
+            if (contador >= 40)
+                printf("A lista esta cheia!\n");
+            else
+                cadastrarAluno(contador, alunos);
             contador++;
             break;
         case 2:
-            listarAlunos(contador, alunos);
+            if (contador > 0)
+                listarAlunos(contador, alunos);
+            else
+                printf("Turma vazia!\n");
             break;
         case 3:
-
-            exibirAluno(contador, alunos);
+            if (contador > 0)
+                exibirAluno(contador, alunos);
+            else
+                printf("Turma vazia!\n");
             break;
         case 4:
-
+            if (contador > 0)
+                maiorMedia(contador, alunos);
+            else
+                printf("Turma vazia!\n");
             break;
         case 5:
-
+            if (contador > 0)
+                mediaDasMedias(contador, alunos);
+            else
+                printf("Turma vazia!\n");
             break;
         case 6:
-
+            if (contador > 0)
+            {
+                excluirAlunos(contador, alunos);
+                contador--;
+            }
+            else
+                printf("Turma vazia!\n");
             break;
         case 7:
             printf("Saindo...");
