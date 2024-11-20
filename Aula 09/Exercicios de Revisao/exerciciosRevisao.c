56333f11cc13290d7f929f346650b8b3a1550a3a
Andriel <andriel.h@aluno.ifsp.edu.br>
2024-11-19 22:05:05 -0300

    Exercicios de Revisao

--- Aula 09/Exercicios de Revisao/exerciciosRevisao.c
+++ Aula 09/Exercicios de Revisao/exerciciosRevisao.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /*1. Um problema típico em computação consiste em converter um
 número da sua forma decimal para a forma binária. Por exemplo, o
@@ -23,7 +24,7 @@ void Dec2Bin(int n)
     Dec2Bin(n);
     printf("%d", n2 % 2);
 }
-/*. Implemente uma função recursiva que
+/*2. Implemente uma função recursiva que
 receba um vetor de inteiros e retorne seu
 maior valor.*/
 
@@ -34,6 +35,39 @@ int maiorDoVetor(int vetor[], int contador)
     int maior = maiorDoVetor(vetor, contador - 1);
     return (vetor[contador - 1] > maior ? vetor[contador - 1] : maior);
 }
+
+/* 3. 
+*/
+typedef struct{
+    char nome[50];
+    int notaS;
+    int notaE
+} Alunos;
+
+Alunos lerAluno(){
+    Alunos aluno;
+    printf("\nInforme o nome do Aluno: ");
+    getchar();
+    fgets(aluno.nome, 50, stdin);
+    printf("\nInforme a nota obtida ao longo do semestre: ");
+    scanf("%d", &aluno.notaS);
+    printf("\nInforme a nota obtida na avaliacao especial: ");
+    scanf("%d",&aluno.notaE);
+
+    return aluno;
+}
+void exibirAluno(Alunos aluno){
+	printf("\t\nNome do Aluno %s", aluno.nome);
+    printf("\t\nNota do aluno ao longo do semestre: %d", aluno.notaE);
+    printf("\t\nNota do aluno na avalicao especial: %d", aluno.notaS); 
+}
+
+void exibirTurma(Alunos alunos[],int qtdAlunos){
+    if(qtdAlunos == 0)
+        return;
+    exibirTurma(alunos, qtdAlunos - 1);
+    exibirAluno(alunos[qtdAlunos]);
+}
 int main()
 {
     // Dec2Bin(12);
@@ -43,7 +77,29 @@ int main()
     // Dec2Bin(3);
 
     int vetor[] = {8, 12, 133, 155, 18, 1, 2, 3, 21, 6};
-    int tam = 10;
+    int tam = 10, op, tamTurma = 10, qtdAlunos;
+    Alunos turma[tamTurma];
+    Alunos temp;
     // printf("%d", maiorDoVetor(vetor, tam));
+    do{
+        printf("\n1, 2 ou 3: ");
+        scanf("%d", &op);
+    switch(op){
+        case 1: 
+                temp = lerAluno();
+                turma[qtdAlunos++] = temp;
+            break;
+        case 2:
+            exibirTurma(turma, qtdAlunos);
+            break;
+        case 3:
+            printf("\nsaindo..");
+            break;
+        default:    
+            printf("\n1 a 3");
+            break;  
+    }
+    }while(op != 3);
+
     return 0;
 }
